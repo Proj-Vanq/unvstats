3ad96a233fa473a72156412f6be2910a1622596f
Darren Salt <devspam@moreofthesa.me.uk>
2013-06-14 14:21:52 +0100

    Hide player sigs for now (they need UTF-8 fixup).

--- web/templates/default/player_details.tpl.php
+++ web/templates/default/player_details.tpl.php
@@ -3,7 +3,7 @@
 <div id="box">
   <div class="heading">
     <span class="heading"><h2>Player Details for <?php echo replace_color_codes($this->player_details['player_name']); ?></h2></span>
-    <span class="headinglink"> ( <a href="player_getsig.php?player_id=<?php echo $this->player_details['player_id'] ?>">get a player signature</a> )</span>
+    <!-- span class="headinglink"> ( <a href="player_getsig.php?player_id=<?php echo $this->player_details['player_id'] ?>">get a player signature</a> )</span -->
   </div>
 
   <table>
