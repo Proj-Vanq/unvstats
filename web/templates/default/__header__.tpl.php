d5a78daa5713ffecff6e14398641163b19838a92
Darren Salt <devspam@moreofthesa.me.uk>
2013-06-15 18:16:44 +0100

    Add the server name to the page title.

--- web/templates/default/__header__.tpl.php
+++ web/templates/default/__header__.tpl.php
@@ -2,7 +2,7 @@
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
   <head>
-    <title>Unvstats</title>
+    <title>Unvstats – <?php echo replace_color_codes(TREMULOUS_SERVER_NAME); ?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="Publisher" content="DASPRiD's" />
     <link rel="stylesheet" type="text/css" href="<?php echo $this->css_file(); ?>" />
